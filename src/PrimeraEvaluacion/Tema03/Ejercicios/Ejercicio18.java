86f0e98a7c7efaae98f369b90202c9cab1b76c7e
ismael.sisa <ismael.sisa@educa.madrid.org>
2023-10-11 17:31:34 +0200

    iSMAEL sisa

--- src/PrimeraEvaluacion/Tema03/Ejercicios/Ejercicio18.java
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/Ejercicio18.java
@@ -2,6 +2,8 @@ package PrimeraEvaluacion.Tema03.Ejercicios;
 
 public class Ejercicio18 {
     public static void main(String[] args) {
-
+        for (int i = 531; i < 541; i++){
+            System.out.println(i + " ");
+        }
     }
 }
