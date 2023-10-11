3e30ad73e2db07b17998475262380a63783ffbea
ismael.sisa <ismael.sisa@educa.madrid.org>
2023-10-11 17:37:50 +0200

    iSMAEL sisa

--- src/PrimeraEvaluacion/Tema03/Ejercicios/Ejercicio19.java
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/Ejercicio19.java
@@ -1,4 +1,13 @@
 package PrimeraEvaluacion.Tema03.Ejercicios;
 
 public class Ejercicio19 {
+    public static void main(String[] args) {
+        int suma = 0;
+
+        for (int numero = 1; numero <= 10; numero++) {
+            suma += numero;
+        }
+
+        System.out.println("La suma de los números enteros del 1 al 10 es: " + suma);
+    }
 }
