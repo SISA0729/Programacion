10dc3a16ae00615e2faf9e8c92b91e9312fe63fe
ismael.sisa <ismael.sisa@educa.madrid.org>
2023-10-11 17:54:06 +0200

    iSMAEL sisa

--- src/PrimeraEvaluacion/Tema03/Ejercicios/Ejercicio20.java
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/Ejercicio20.java
@@ -1,4 +1,19 @@
 package PrimeraEvaluacion.Tema03.Ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio20 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Número a: ");
+        int n1 = teclado.nextInt();
+        System.out.print("Número b: ");
+        int n2 = teclado.nextInt();
+
+        int suma = 0;
+        for (int i= n1; i <= n2; i++){
+            suma += i;
+        }
+        System.out.println(suma);
+    }
 }
