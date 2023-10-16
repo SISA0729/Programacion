9f8315faacf0492177dac49c2d38112730befbd3
ismael.sisa <ismael.sisa@educa.madrid.org>
2023-10-16 16:37:21 +0200

    iSMAEL sisa

---
+++ src/PrimeraEvaluacion/Tema02/ejemplos/variables/EjemplosRambom.java
@@ -0,0 +1,11 @@
+package PrimeraEvaluacion.Tema02.ejemplos.variables;
+
+import java.util.Random;
+
+public class EjemplosRambom {
+    public static void main(String[] args) {
+        Random generador = new Random();
+        int numeroaleatorio = generador.nextInt();
+
+    }
+}
