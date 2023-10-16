bcebe5d4dd4d01524e592913463f2ecb8d9f1090
ismael.sisa <ismael.sisa@educa.madrid.org>
2023-10-16 16:59:06 +0200

    iSMAEL sisa

--- src/PrimeraEvaluacion/Tema02/ejemplos/variables/EjemplosRambom.java
+++ src/PrimeraEvaluacion/Tema02/ejemplos/variables/EjemplosRambom.java
@@ -5,7 +5,20 @@ import java.util.Random;
 public class EjemplosRambom {
     public static void main(String[] args) {
         Random generador = new Random();
-        int numeroaleatorio = generador.nextInt();
+        int numeroaleatorio = generador.nextInt(10);
+        // vamos a generar y mostrar 100 numeros aleatorios con un for
+        for (int i =0; i < 100; i++){
+            System.out.print(numeroaleatorio + " ");
+            numeroaleatorio = generador.nextInt(0, 11);
+            // en general, si quieres generar nuemeros ente min y max
+            // haces generador.nestInt(min, max + 1)
+
+        }
+         //queremos generar 50 numeros
+        for (int i = 0; i < 50;i++ ){
+            System.out.print(generador.nextInt(-10,11) + " ");
+        }
+
 
     }
 }
