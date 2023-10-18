741ededb744c9f0efcac917a12074520bedfae01
ismael.sisa <ismael.sisa@educa.madrid.org>
2023-10-18 19:16:23 +0200

    iSMAEL sisa

--- src/PrimeraEvaluacion/Tema03/Ejercicios/Ejercicio26.java
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/Ejercicio26.java
@@ -3,9 +3,12 @@ package PrimeraEvaluacion.Tema03.Ejercicios;
 public class Ejercicio26 {
     public static void main(String[] args) {
         int suma = 0;
+        int producto = 1;
         for (int i = 1; i < 31; i++) {
             suma = suma + i;
+            producto = producto *i;
         }
-        System.out.println(suma);
+        System.out.println("Suma = " + suma);
+        System.out.println("Producto = " + producto);
     }
 }
