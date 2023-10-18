5d2778bee6b4923afd0974f124474321bb7dea0a
ismael.sisa <ismael.sisa@educa.madrid.org>
2023-10-18 19:12:48 +0200

    iSMAEL sisa

---
+++ src/PrimeraEvaluacion/Tema03/Ejercicios/Ejercicio26.java
@@ -0,0 +1,11 @@
+package PrimeraEvaluacion.Tema03.Ejercicios;
+
+public class Ejercicio26 {
+    public static void main(String[] args) {
+        int suma = 0;
+        for (int i = 1; i < 31; i++) {
+            suma = suma + i;
+        }
+        System.out.println(suma);
+    }
+}
