9f0cc84246b418bc7f38d40cd1b03540ef763503
ismael.sisa <ismael.sisa@educa.madrid.org>
2023-10-23 17:17:03 +0200

    iSMAEL sisa

---
+++ src/PrimeraEvaluacion/Tema03/Ejemplos/EejemplosDoWhile.java
@@ -0,0 +1,12 @@
+package PrimeraEvaluacion.Tema03.Ejemplos;
+
+public class EejemplosDoWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        }while (i < 11);
+
+    }
+}
