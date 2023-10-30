8854a40f299f6a13a3651ff9c5a58cb174323d47
ismael.sisa <ismael.sisa@educa.madrid.org>
2023-10-30 17:03:31 +0100

    Ismael SISA

---
+++ src/PrimeraEvaluacion/Tema04/Ejemplos/EjemplosString.java
@@ -0,0 +1,13 @@
+package PrimeraEvaluacion.Tema04.Ejemplos;
+
+public class EjemplosString {
+    public static void main(String[] args) {
+        String srt = null;
+        System.out.println(srt);
+        srt= "";
+        System.out.println(srt);
+        srt = "hola";
+        System.out.println(srt);
+
+    }
+}
