c8bbfea152e82829db48576943e2f82e665779f3
ismael.sisa <ismael.sisa@educa.madrid.org>
2023-11-15 19:29:14 +0100

    Ismael SISA

---
+++ src/PrimeraEvaluacion/Retos/P247.java
@@ -0,0 +1,21 @@
+package PrimeraEvaluacion.Retos;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P247 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int n =   teclado.nextInt();
+        teclado.nextLine();
+        while (n != 0){
+            String linea = teclado.nextLine();
+            String[] arraylinea = linea.split(" ");
+            System.out.println(Arrays.toString(arraylinea));
+            n = teclado.nextInt();
+            teclado.nextLine();
+            
+        }
+        teclado.close();
+    }
+}
