f8c375f63e2267a658b1740edda6e238fc5f483c
ismael.sisa <ismael.sisa@educa.madrid.org>
2024-03-08 11:02:30 +0100

    ISMAEL SISAas

--- src/SegundaEvaluacion/zelectrodomesticos/Electrodomestico.java
+++ src/SegundaEvaluacion/zelectrodomesticos/Electrodomestico.java
@@ -1,4 +1,4 @@
-package zelectrodomesticos;
+package SegundaEvaluacion.zelectrodomesticos;
 
 public class Electrodomestico {
     // Propiedades
--- src/SegundaEvaluacion/zelectrodomesticos/Main.java
+++ src/SegundaEvaluacion/zelectrodomesticos/Main.java
@@ -1,4 +1,4 @@
-package zelectrodomesticos;
+package SegundaEvaluacion.zelectrodomesticos;
 
 public class Main {
     public static void main(String[] args) {
